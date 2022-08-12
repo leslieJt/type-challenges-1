30fb6debe55a201fb0cd8a7404b86acbad8fff38
Cantius Chew <55541825+ethereal-sheep@users.noreply.github.com>
2022-08-12 11:16:23 +0000

    feat(question): add #14427 - printf 2

---
+++ questions/14427-hard-printf-2/test-cases.ts
@@ -0,0 +1,12 @@
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Printf<'%s %s', ['Hello', 'World']>, 'Hello World'>>,
+  Expect<Equal<Printf<'%s %d %s %b', ['All', 3, 'types', true]>, 'All 3 types true'>>,
+  Expect<Equal<Printf<'Invalid', [true]>, never>>,
+  Expect<Equal<Printf<'Invalid %b', []>, never>>,
+  Expect<Equal<Printf<'Bad Order %d %s %b', [true, 1, 's']>, never>>,
+  Expect<Equal<Printf<'Valid %%%b', [true]>, 'Valid %true'>>,
+  Expect<Equal<Printf<'Convoluted %d%d%%%d%%%d %d', [1,2,3,4,5]>, 'Convoluted 12%3%4 5'>>,
+  Expect<Equal<Printf<'Seems Wrong %%s %%d %%b', []>, 'Seems Wrong %s %d %b'>>,
+]
